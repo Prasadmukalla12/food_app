9c35860e582fa1ce50ce661d061ffde53aa5aae5
PrasadMukalla <prasadmukalla124@gmail.com>
2026-05-11 16:49:36 +0530

    Final update

--- src/components/CartItems.jsx
+++ src/components/CartItems.jsx
@@ -1,4 +1,4 @@
-import { lazy, useCallback, useEffect, useState } from "react"
+import React, { lazy, useCallback, useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { Link, useNavigate } from "react-router-dom"
 import Swal from "sweetalert2"
